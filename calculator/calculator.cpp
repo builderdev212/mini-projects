fcb9f465092e8465c2b5ae634768fedc024eacbe
builderdev212 <builderdev212@users.noreply.github.com>
2022-11-03 19:58:25 -0400

    Actually fix up simple calculator.

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
 using namespace std;
 
+// Menu constants.
 const int ADD = 1;
 const int SUB = 2;
 const int MULT = 3;
 const int DIV = 4;
 const int EXIT = 5;
 
+// Function to do the menu.
+int userChoice();
+
 int main() {
   // Variables to store options
   int a;
   int b;
   int choice;
-  int running = true;
+  bool running = true;
 
   do {
-    bool first = true;
-    do {
-      if (!first) {
-        cout << "Error. Please enter a number between " << ADD << " and " << EXIT << ".\n" << endl;
-      }
-      cout << ADD << ". Add two numbers." << endl;
-      cout << SUB << ". Subtract two numbers." << endl;
-      cout << MULT << ". Multiply two numbers." << endl;
-      cout << DIV << ". Divide two numbers." << endl;
-      cout << EXIT << ". Exit the program." << endl;
-      cin >> choice;
-    } while ((choice < ADD) || (choice > EXIT));
+    choice = userChoice();
 
     // Action based on the users input.
     switch (choice) {
@@ -78,4 +71,29 @@ int main() {
   getchar();
 #endif
   return 0;
+}
+
+// Function to do the menu.
+int userChoice() {
+  // Varaible to store the users input.
+  int choice;
+  // Error message validation.
+  bool first = true;
+
+  // Loop through until the user enters a valid choice.
+  do {
+    // If this has ran more than once, then display an error message.
+    if (!first) {
+      cout << "Error. Please enter a number between " << ADD << " and " << EXIT << ".\n" << endl;
+    }
+    cout << ADD << ". Add two numbers." << endl;
+    cout << SUB << ". Subtract two numbers." << endl;
+    cout << MULT << ". Multiply two numbers." << endl;
+    cout << DIV << ". Divide two numbers." << endl;
+    cout << EXIT << ". Exit the program." << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+  } while ((choice < ADD) || (choice > EXIT));
+
+  return choice;
 }
